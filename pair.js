e6d16b64dbcf13f2f5803782aab0d0bb4f226b6e
Madmkisii <138007129+Madmkisii@users.noreply.github.com>
2024-05-10 22:13:49 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -61,14 +61,14 @@ ____________________________
         𝐃𝐑𝐄𝐗-𝐌𝐃-𝐕2
 ____________________________
 ╔════◇
-║『 *YOU'VE CHOSEN DREX-MD-V2* 』
+║『 *YOU'VE CHOSEN MEGATRON* 』
 ║ You've Completed the First Step
 ║ to Deploy a Whatsapp Bot.
 ╚════════════════╝
 ╔═════◇
 ║ 『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ 𝐎𝐰𝐧𝐞𝐫: https://wa.me/254102074064
-║❒ 𝐑𝐞𝐩𝐨: https://github.com/drexmose/drex-md-v2
+║❒ 𝐎𝐰𝐧𝐞𝐫: https://wa.me/254790593618
+║❒ 𝐑𝐞𝐩𝐨: https://github.com/Madmkisii/Megatron-Bot
 ║❒ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: https://whatsapp.com/channel/0029Vabz7vUJ3jutx2fNsf2P
 ║ 💜💜💜
 ╚════════════════╝
